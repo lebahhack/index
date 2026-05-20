e299ca05adff53c62d644dd34579adb9b30c3479
lebahhack <lebahhack@gmail.com>
2026-05-21 04:26:05 +0700

    Add files via upload

---
+++ src/lib/seo.ts
@@ -0,0 +1,16 @@
+
+import { SITE } from "./site";
+
+export function buildSeo({
+  title,
+  description,
+  image,
+  canonical
+}) {
+  return {
+    title: title || SITE.title,
+    description: description || SITE.description,
+    image: image || SITE.image,
+    canonical: canonical || SITE.url()
+  };
+}
